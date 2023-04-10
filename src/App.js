9ffd98e6199cd300879c1454017e416c738e6b5a
MD Iftekhar Ferdous <mustakratan1987@gmail.com>
2023-04-11 05:08:26 +0600

    Crated app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React from 'react';
-// import logo from './logo.svg';
+import Navbar from './components/Navbar.js';
 import './App.css';
 
 function App() {
   
   return (
     <>
-   <nav>
-    <ul className="flex space-x-4">
-     <li>Navbar</li>
-     <li>Navbar</li>
-     <li>Navbar</li>
-    </ul>
-   </nav>
+  <Navbar/>
   </>
   )
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import '../index.css'
+function Comp() {
+  return (
+  <nav>
+    <ul className="flex space-x-4 bg-amber-600">
+      <li className="p-5">Home </li>
+      <li className="p-5">About </li>
+      <li className="p-5">Contact</li>
+    </ul>
+  </nav>  
+  )
+}
+
+export default Comp;
